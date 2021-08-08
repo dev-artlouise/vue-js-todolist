0e2c15394471420880f238da0fbc5cf08efcfbc8
dev-artlouise <dev.artlouises@gmail.com>
2021-08-08 12:45:54 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+    <!-- Reuse components with different props -->
+    <!-- <Button text="Update Task" color="Blue" />
+    <Button text="Delete Task" color="red" /> -->
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+
+  //normal implementation
+  // props: {
+  //   title: String,
+  // },
+
+  //With Default Value
+  props: {
+    title: {
+      type: String,
+      default: "Hello World",
+    },
+  },
+
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+Header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+}
+</style>
